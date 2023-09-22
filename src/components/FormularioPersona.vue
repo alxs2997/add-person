010a9b88c367ae386cd5409ed456427a6036343e
Alexis Gallegos <98421465+alxs2997@users.noreply.github.com>
2023-09-22 12:19:11 -0600

    añadiendo función para eliminar y modificando datos en tabla

--- src/components/FormularioPersona.vue
+++ src/components/FormularioPersona.vue
@@ -1,35 +1,46 @@
 <template>
-  <div id="formulario-persona" @add-persona="agregarPersona">
+  <div id="formulario-persona">
     <form @submit.prevent="enviarFormulario">
       <div class="container">
         <div class="row">
           <div class="col-md-4">
             <div class="form-group">
-              <label>Nombre</label>
+              <label>Nombre:</label>
               <input
+                ref="nombre"
                 v-model="persona.nombre"
                 type="text"
                 class="form-control"
+                :class="{ 'is-invalid': procesando && nombreInvalido }"
+                @focus="resetEstado"
+                @keypress="resetEstado"
+                placeholder="Juan"
               />
             </div>
           </div>
           <div class="col-md-4">
             <div class="form-group">
-              <label>Apellido</label>
+              <label>Apellido:</label>
               <input
                 v-model="persona.apellido"
                 type="text"
                 class="form-control"
+                :class="{ 'is-invalid': procesando && apellidoInvalido }"
+                @focus="resetEstado"
+                placeholder="Castañeda"
               />
             </div>
           </div>
           <div class="col-md-4">
             <div class="form-group">
-              <label>Email</label>
+              <label>Email:</label>
               <input
                 v-model="persona.email"
                 type="email"
                 class="form-control"
+                :class="{ 'is-invalid': procesando && emailInvalido }"
+                @focus="resetEstado"
+                placeholder="correo@email.com"
               />
             </div>
           </div>
@@ -37,11 +48,23 @@
         <div class="row">
           <div class="col-md-4">
             <div class="form-group">
-              <button class="btn btn-primary">Añadir persona</button>
+              <button class="btn-env" title="Añadir Persona">Añadir persona</button>
             </div>
           </div>
         </div>
       </div>
+      <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div v-if="error && procesando" class="alert alert-danger" role="alert">
+          Debes rellenar todos los campos!
+        </div>
+        <div v-if="correcto" class="alert alert-success" role="alert">
+          La persona ha sido agregada correctamente!
+        </div>
+      </div>
+    </div>
+  </div>
     </form>
   </div>
 </template>
@@ -51,15 +74,55 @@ export default {
   name: "formulario-persona",
   data() {
     return {
+      procesando: false,
+      correcto: false,
+      error: false,
       persona: {
         nombre: "",
         apellido: "",
+        email: "",
       },
     };
   },
   methods: {
     enviarFormulario() {
+      this.procesando = true;
+      this.resetEstado();
+
+      // Comprobamos la presencia de errores
+      if (this.nombreInvalido || this.apellidoInvalido || this.emailInvalido) {
+        this.error = true;
+        return;
+      }
+
       this.$emit("add-persona", this.persona);
+      this.$refs.nombre.focus();
+
+      this.error = false;
+      this.correcto = true;
+      this.procesando = false;
+
+      // Restablecemos el valor de la variables
+      this.persona = {
+        nombre: "",
+        apellido: "",
+        email: "",
+      };
+    },
+    resetEstado() {
+      this.correcto = false;
+      this.error = false;
+    },
+  },
+  computed: {
+    nombreInvalido() {
+      return this.persona.nombre.length < 1;
+    },
+    apellidoInvalido() {
+      return this.persona.apellido.length < 1;
+    },
+    emailInvalido() {
+      return this.persona.email.length < 1;
     },
   },
 };
@@ -68,5 +131,27 @@ export default {
   <style scoped>
 form {
   margin-bottom: 2rem;
+  border: #07642e 1px solid;
+  border-radius: 20px;
+}
+
+.container {
+  padding: 1rem;
+}
+
+input {
+  border-radius: 20px;
+}
+.btn-env {
+  color: #07642e;
+  background: #fff;
+  border-radius: 20px;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+}
+.btn-env:hover {
+  background-color: #07642e;
+  color: #fff;
+  border-radius: 20px;
 }
 </style>
--- src/components/TablaPersonas.vue
+++ src/components/TablaPersonas.vue
@@ -1,34 +1,76 @@
 <template>
-<div id="tabla-personas">
+  <div v-if="!personas.length" class="alert alert-info" role="alert">
+  No se han agregado personas
+</div>
+  <div id="tabla-personas">
     <table class="table">
       <thead>
-        <tr>
+        <tr class="acciones">
           <th>Nombre</th>
           <th>Apellido</th>
           <th>Email</th>
+          <th>Acciones</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="persona in personas" :key="persona.id">
-          <td>{{ persona.nombre}}</td>
+        <tr class="content" v-for="persona in personas" :key="persona.id">
+          <td>{{ persona.nombre }}</td>
           <td>{{ persona.apellido }}</td>
-          <td>{{ persona.email}}</td>
+          <td>{{ persona.email }}</td>
+          <td>
+              <button class="btn-eliminar"
+              title="Eliminar persona"
+              @click="$emit('delete-persona', persona.id)"
+              ><i class='bx bxs-eraser'></i></button>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
   
-  <script>
+<script>
 export default {
-    name: "TablaPersonas",
-    props: {
-        personas: {
-        type: Array,
-        required: true,
-        },
-    },
+  name: "tabla-personas",
+  props: {
+    personas: Array,
+  },
 };
 </script>
   
-  <style scoped></style>
+<style scoped>
+#tabla-personas {
+  margin-top: 20px;
+}
+
+#tabla-personas table {
+  width: 100%;
+}
+
+#tabla-personas table thead {
+  background-color: #f5f5f5;
+}
+
+.acciones {
+  text-align: center;
+}
+
+.content {
+  text-align: center;
+}
+
+.btn-eliminar {
+  background: none;
+  border: #f03f3f solid 1px;
+  color: #f03f3f;
+  padding: 5px 10px;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.btn-eliminar:hover {
+  background: #f03f3f83;
+  color: #fff;
+}
+
+</style>
