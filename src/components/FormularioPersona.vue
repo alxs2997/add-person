8c7a5893ab453a47d33dca217a1aee85ce9c52c6
Alexis Gallegos <98421465+alxs2997@users.noreply.github.com>
2023-09-21 21:13:18 -0600

    iniciando aplicación de añadir personas

---
+++ src/components/FormularioPersona.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="formulario-persona" @add-persona="agregarPersona">
+    <form @submit.prevent="enviarFormulario">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Nombre</label>
+              <input
+                v-model="persona.nombre"
+                type="text"
+                class="form-control"
+              />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Apellido</label>
+              <input
+                v-model="persona.apellido"
+                type="text"
+                class="form-control"
+              />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Email</label>
+              <input
+                v-model="persona.email"
+                type="email"
+                class="form-control"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <button class="btn btn-primary">Añadir persona</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "formulario-persona",
+  data() {
+    return {
+      persona: {
+        nombre: "",
+        apellido: "",
+      },
+    };
+  },
+  methods: {
+    enviarFormulario() {
+      this.$emit("add-persona", this.persona);
+    },
+  },
+};
+</script>
+  
+  <style scoped>
+form {
+  margin-bottom: 2rem;
+}
+</style>
---
+++ src/components/TablaPersonas.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="tabla-personas">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="persona in personas" :key="persona.id">
+          <td>{{ persona.nombre}}</td>
+          <td>{{ persona.apellido }}</td>
+          <td>{{ persona.email}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+  
+  <script>
+export default {
+    name: "TablaPersonas",
+    props: {
+        personas: {
+        type: Array,
+        required: true,
+        },
+    },
+};
+</script>
+  
+  <style scoped></style>
